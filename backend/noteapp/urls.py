4b73c12139d75efe6a246c557d600e0503182453
Evans Bamfo Boateng <eb5337877@gmail.com>
2024-09-16 19:00:50 -0700

    Added the view and url for update and delete of each note

--- backend/noteapp/urls.py
+++ backend/noteapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import NoteCreateView, NotesListView
+from .views import *
 
 urlpatterns = [
     
     path('api/notecreate/', NoteCreateView.as_view(), name='note-create'),
     path('api/notes/', NotesListView.as_view(), name='notes-list'),
+    path('api/updatedelete/<str:pk>/', UpdateDeleteView.as_view(), name='update-delete'),
 ]
--- backend/noteapp/views.py
+++ backend/noteapp/views.py
@@ -14,21 +14,11 @@ class NotesListView(generics.ListAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
-    # def list(self, request, *args, **kwargs):
-    #     queryset = self.filter_queryset(self.get_queryset())
-
-    #     page = self.paginate_queryset(queryset)
-    #     if page is not None:
-    #         serializer = self.get_serializer(page, many=True)
-    #         return self.get_paginated_response(serializer.data)
-
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     return Response(serializer.data)
-
-    # def get(self, request, *args, **kwargs):
-    #     return self.list(request, *args, **kwargs)
-
     # def get_queryset(self):
     #     return Note.objects.filter(user=self.request.user)
+
+class UpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
 
 
