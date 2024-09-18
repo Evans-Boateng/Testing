2067e4221629e8892f80811b1813fe76b34ea047
Evans Bamfo Boateng <eb5337877@gmail.com>
2024-09-18 16:58:08 -0700

    modified the update note modal

--- frontend/src/UpdateNoteModal.jsx
+++ frontend/src/UpdateNoteModal.jsx
@@ -47,9 +47,9 @@ export default function UpdateNote({open,onClose,noteId}) {
               <svg className="w-[12px] text-[#8a8f8b]" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
             </button>
           </div>
-          <input value={noteData.title} name="name" onChange={handleInputChange} className="w-full h-8 font-medium text-lg mb-[5px] focus:outline-none" />
+          <input value={noteData.title} name="title" onChange={handleInputChange} className="w-full h-8 font-medium text-lg mb-[5px] focus:outline-none" />
           <span className="block text-[12px] mb-[5px]">CONTENT</span>
-          <textarea value={noteData.content} onChange={handleInputChange} rows="4" className="w-full bg-[#f5f5f5] rounded focus:outline-none px-1 py-[4px] font-medium text-sm mb-4" />
+          <textarea value={noteData.content} name="content" onChange={handleInputChange} rows="4" className="w-full bg-[#f5f5f5] rounded focus:outline-none px-1 py-[4px] font-medium text-sm mb-4" />
 
 
           <div className="flex justify-center">
