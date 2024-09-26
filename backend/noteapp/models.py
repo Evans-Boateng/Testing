4d8c28f867c1ca5d817473dd894c024fa8c7957e
Evans Bamfo Boateng <eb5337877@gmail.com>
2024-09-26 07:54:43 -0700

    Added the custom user model

--- backend/noteapp/models.py
+++ backend/noteapp/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.name
 
     
 class Note(models.Model):
