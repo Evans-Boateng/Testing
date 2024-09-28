0dc93ae8a56e6eb24f5e1b40dd517d945966fc56
Evans Bamfo Boateng <eb5337877@gmail.com>
2024-09-27 18:11:12 -0700

    Implemented jwt authentication and create the views and endpoints the login page

--- backend/noteapp/urls.py
+++ backend/noteapp/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import *
 
@@ -8,4 +12,6 @@ urlpatterns = [
     path('api/notes/', NotesListView.as_view(), name='notes-list'),
     path('api/updatedelete/<str:pk>/', UpdateDeleteView.as_view(), name='update-delete'),
     path('api/usercreate/', CreateUserView.as_view(), name='createuser'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('api/token/', LoginView.as_view(), name="access-token")
 ]
--- backend/noteapp/views.py
+++ backend/noteapp/views.py
@@ -4,10 +4,39 @@ from .models import  Note, User
 from rest_framework.response import Response
 from .serializers import  NoteSerializer, CreateUserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.http import JsonResponse
 
-# Create your views here.
+class LoginView(TokenObtainPairView):
+    
+    serializer_class = TokenObtainPairSerializer
+    permission_classes = [AllowAny]
 
-class CreateUserView(generics.ListCreateAPIView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        data = response.data
+        refresh_token = data.get('refresh')
+        
+        # Create a new response object
+        response = JsonResponse(data)
+        
+        # Set the refresh token in an HttpOnly cookie
+        response.set_cookie(
+            key='refresh_token',
+            value=refresh_token,
+            httponly=True,
+            secure=True,  # Use secure=True in production
+            samesite='Strict'  # Adjust SameSite attribute as needed
+        )
+        
+        # Remove the refresh token from the response body
+        del data['refresh']
+        
+        return response
+
+
+class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = [AllowAny]
@@ -21,8 +50,8 @@ class NotesListView(generics.ListAPIView):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
-    # def get_queryset(self):
-    #     return Note.objects.filter(user=self.request.user)
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
 
 class UpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.all()
