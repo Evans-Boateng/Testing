e2b0f7d915ebcc9fbde4de5ce5ba72ef6e725ef1
Evans Bamfo Boateng <eb5337877@gmail.com>
2024-11-03 03:02:19 -0800

    created an intial component that will rendered when users don't have notes and a notes context that handles all requests in the application

---
+++ frontend/src/Initial.jsx
@@ -0,0 +1,8 @@
+export default function Initial(){
+    return (
+        <div className="flex flex-col justify-center items-center w-full h-full pt-10">
+            <img className="w-[200px]" src="src\assets\add-note-illustration.svg"/>
+            <p className="text-sm text-center ">You currently do not have any notes. Click on the add button to create notes.</p>
+        </div>
+    )
+}
---
+++ frontend/src/NotesContext.jsx
@@ -0,0 +1,76 @@
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuth } from './AuthProvider';
+
+const NotesContext = createContext();
+
+
+export const NotesProvider = ({children}) => {
+  const {token} = useAuth();
+  const [notes, setNotes] = useState([]); 
+  useEffect(() => {
+    if (token){
+      fetchNotes();
+    }   
+  }, [token])
+
+  const fetchNotes = async () => {
+    try{
+      const response = await axios.get("http://127.0.0.1:8000/api/notes/", {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }  
+      })
+      setNotes(response.data)
+    }catch(error){
+      console.error('Error in fetching notes', error)
+    }
+  }
+  const handleUpdateNote = async (updatedNote) => {
+    try{
+      const response = await axios.patch(`http://127.0.0.1:8000/api/updatedelete/${updatedNote.id}/`, updatedNote, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      if (response.status === 200) {
+        setNotes((prevNotes) => 
+          prevNotes.map((note) => 
+            note.id === updatedNote.id ? updatedNote: note
+          )
+        )
+      }
+
+    }
+    catch(error){
+      console.error('error in updating note ', error)
+    }
+    
+  };
+
+  const handleDeleteNote = async (noteId) => {
+    try{
+      const response = await axios.delete(`http://127.0.0.1:8000/api/updatedelete/${noteId}/`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+  
+      if (response.status === 200) {
+        setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId));
+      }
+    } catch (error) {
+      console.error("Error in updating note", error)
+    }
+
+  };
+
+  return (
+    <NotesContext.Provider 
+      value={{notes, fetchNotes, handleUpdateNote, handleDeleteNote}}
+    >
+      {children}
+    </NotesContext.Provider>
+  )
+}
+export const useNotes = () => useContext(NotesContext);
