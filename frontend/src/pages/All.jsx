21ec04e1832c5fddd5da032b1c5e57eb682645cd
Evans Bamfo Boateng <eb5337877@gmail.com>
2024-11-22 10:38:58 -0800

    Now the initial component is displayed when the user has not created any notes

--- frontend/src/pages/All.jsx
+++ frontend/src/pages/All.jsx
@@ -2,38 +2,29 @@ import { useEffect, useState } from "react";
 import Note from "../note";
 import axios from "axios";
 import { useAuth } from "../AuthProvider";
+import { useNotes } from "../NotesContext";
+import Initial from "../Initial";
 
 
 
 export default function All() {
-    const [notes, setNotes] = useState([])
-    const {token} = useAuth();
-    useEffect(() => {
-        const fetchNotes = async () =>{
-            try{
-                const response = await axios.get("http://127.0.0.1:8000/api/notes/",{
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                    
-                })
-                setNotes(response.data)
-                
-            }
-            catch (error) {
-                console.error("error in fetching notes", error.response?.data || error.message)
-            }
-        }
-        fetchNotes();
-    }, [])
+
+    const {notes} = useNotes();
+      
 
     return (
         <>
-            {
-                notes.map((note, index) =>{
-                    return <Note title={note.title} content={note.content} category={note.category} created={note.created} key={index}
-                        id={note.id}/>
-                })
+            {   
+                notes.length === 0 ? <Initial /> :
+                <div className="grid sm:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-5">
+                    {notes.map((note, index) => {
+                        return (
+                            
+                            <Note title={note.title} content={note.content} category={note.category} created={note.created} note={note} key={note.id}
+                            id={note.id}/>
+                        )
+                    })}
+                </div>
             }
 
         </>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,38 +2,35 @@ import axios from "axios";
 import Note from "../note"
 import { useEffect, useState } from "react";
 import { useAuth } from "../AuthProvider";
+import Initial from "../Initial";
+import { useNotes } from "../NotesContext";
 
 
 export default function Home() {
-  const [notes, setNotes] = useState([]);
-  const {token} = useAuth();
-  useEffect(() => {
-    const fetchNotes = async () =>{
-      try{
-        const response = await axios.get("http://127.0.0.1:8000/api/notes/",{
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
-        setNotes(response.data)
-      }
-      catch (error) {
-        console.error("error in fetching note", error)
-      }
-      
-    }
-    fetchNotes();
-  }, [])
+  const {notes} = useNotes()
 
   const filteredNotes = notes.filter((note) => note.category === "Home");
 
   return (
       <>
-        {
-          filteredNotes.map((note, index) => {
-            return <Note title={note.title} content={note.content} category={note.category} created={note.created} key={index}
-            id={note.id}/>
-          })
+        { filteredNotes.length === 0 ? <Initial /> :
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 grid-flow-row gap-5">
+            {filteredNotes.map((note) => {
+              const gotNote = note;
+              return (
+                  <Note 
+                    title={note.title} 
+                    content={note.content} 
+                    category={note.category} 
+                    created={note.created} 
+                    key={note.id}
+                    note={gotNote}
+                    id={note.id}
+                  />
+
+              )
+            })}
+          </div>
         }
       </>
   )
